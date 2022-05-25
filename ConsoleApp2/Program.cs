1dbb57397a7775551f9a6af1d2970890c5081629
Damiasin <damian.wal@interia.pl>
2022-05-25 13:19:21 +0200

    OK

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace ConsoleApp2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string footerText = "footerText";
+            DotFooter dotfooter = new DotFooter();
+            dotfooter.PrintFooterFormatted(footerText);
+
+            DashFooter dashfooter = new DashFooter();
+            dashfooter.PrintFooterFormatted(footerText);
+
+            StarFooter starfooter = new StarFooter();
+            starfooter.PrintFooterFormatted(footerText);
+            Console.ReadKey();
+        }
+
+    }
+
+
+    public class DotFooter : IPrintable
+    {
+        public void PrintFooterFormatted(string footerText)
+        {
+            string a = ".....";
+                Console.WriteLine(a + footerText + a);
+
+        }
+    }
+
+
+    public class DashFooter : IPrintable
+    {
+        string b = "-----";
+       public  void PrintFooterFormatted(string footerText)
+        {
+            Console.WriteLine(b + footerText+ b);
+        }
+    }
+
+
+    public class StarFooter : IPrintable
+    {
+        string c = "*****";
+        public void PrintFooterFormatted(string footerText)
+        {
+            Console.WriteLine(c + footerText + c);
+        }
+    }
+}
---
+++ Polimorfizm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DotFooter dotfooter = new DotFooter();
+            DashFooter dashfooter = new DashFooter();
+            StarFooter starfooter = new StarFooter();
+            Klasa(dotfooter);
+            Klasa(dashfooter);
+            Klasa(starfooter);
+
+            Console.ReadKey();
+
+        }
+        private static void Klasa(IPrintable printable)
+        {
+            string footerText = "footerText";
+            printable.PrintFooterFormatted(footerText);
+
+        }
+
+    }
+    public class DotFooter : IPrintable
+    {
+        public void PrintFooterFormatted(string footerText)
+        {
+            string a = ".....";
+                Console.WriteLine(a + footerText + a);
+
+        }
+    }
+    public class DashFooter : IPrintable
+    {
+        string b = "------";
+       public  void PrintFooterFormatted(string footerText)
+        {
+            Console.WriteLine(b + footerText+ b);
+        }
+    }
+    public class StarFooter : IPrintable
+    {
+        string c = "*****";
+        public void PrintFooterFormatted(string footerText)
+        {
+            Console.WriteLine(c + footerText + c);
+        }
+    }
+}
